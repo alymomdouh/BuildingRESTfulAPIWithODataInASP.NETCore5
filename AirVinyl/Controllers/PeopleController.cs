ad853da8703441d1de886855db6afa5bf8d47932
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2022-12-24 18:26:45 +0200

    Getting an Individual Entity

--- AirVinyl/Controllers/PeopleController.cs
+++ AirVinyl/Controllers/PeopleController.cs
@@ -6,19 +6,30 @@ using System;
 using System.Threading.Tasks;
 
 namespace AirVinyl.Controllers
-{ 
+{
     public class PeopleController : ODataController
     {
         private readonly AirVinylDbContext dbContext;
 
-        public PeopleController(AirVinylDbContext dbContext )
+        public PeopleController(AirVinylDbContext dbContext)
         {
-            this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext)); 
+            this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
         }
         // #RequestInPostman  http://localhost:5000/odata/People
         public async Task<IActionResult> Get()
         {
-             return Ok(await dbContext.People.ToListAsync());
+            return Ok(await dbContext.People.ToListAsync());
+        }
+        // People(1)
+        //get http://localhost:5000/odata/People(1)
+        public async Task<IActionResult> Get(int key)
+        {
+            var person = await dbContext.People.FirstOrDefaultAsync(p => p.PersonId == key);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
         }
     }
 }
--- AirVinyl/Controllers/VinylRecordsController.cs
+++ AirVinyl/Controllers/VinylRecordsController.cs
@@ -25,5 +25,16 @@ namespace AirVinyl.Controllers
         {
             return Ok(await dbContext.VinylRecords.ToListAsync());
         }
+        //get http://localhost:5000/odata/VinylRecords(1)
+        [HttpGet("VinylRecords({key})")]
+        public async Task<IActionResult> GetOneVinylRecord(int key)
+        {
+            var vinylRecord = await dbContext.VinylRecords.FirstOrDefaultAsync(p => p.VinylRecordId == key);
+            if (vinylRecord == null)
+            {
+                return NotFound();
+            }
+            return Ok(vinylRecord);
+        }
     }
 }
